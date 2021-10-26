cf44286a69b128cb74c5b50f6f86396672d0bd13
Vladimir Bessonov <bessmy@gmail.com>
2021-10-26 21:09:03 +0300

    feat: snippet to make an exact copy for 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,6 +5,16 @@ const newFolderPath = path.join(__dirname, 'files-copy');
 const baseFolderPath = path.join(__dirname, 'files');
 
 async function copyFolder() {
+  // If we need to make an exact copy of the folder
+
+  // if (fs.access(newFolderPath)) {
+  //   const oldCopyFiles = await fs.readdir(newFolderPath);
+  //   oldCopyFiles.forEach(async (file) => {
+  //     const pathToFile = path.join(newFolderPath, file);
+  //     fs.unlink(pathToFile);
+  //   });
+  // }
+
   await fs.mkdir(newFolderPath, { recursive: true });
   const files = await fs.readdir(baseFolderPath);
 
