66fc3851038ed0324ac5e92ab6b44e8736f4cc53
Vladimir Bessonov <bessmy@gmail.com>
2021-10-30 13:29:24 +0300

    feat: add 06-build-page

---
+++ 06-build-page/index.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const fsProm = require('fs/promises');
+const path = require('path');
+
+const pathToCss = path.join(__dirname, 'styles');
+const pathToHtml = path.join(__dirname, 'template.html');
+const pathToAssets = path.join(__dirname, 'assets');
+const pathToComponents = path.join(__dirname, 'components');
+const pathToCssBundle = path.join(__dirname, 'project-dist', 'style.css');
+const pathToAssetsBundle = path.join(__dirname, 'project-dist', 'assets');
+const pathToHtmlBundle = path.join(__dirname, 'project-dist', 'index.html');
+
+let htmlFile = '';
+
+async function createHtmlBundle() {
+  const articles = await fsProm.readFile(path.join(pathToComponents, 'articles.html'));
+  const footer = await fsProm.readFile(path.join(pathToComponents, 'footer.html'));
+  const header = await fsProm.readFile(path.join(pathToComponents, 'header.html'));
+
+  const readable = fs.createReadStream(pathToHtml, 'utf8');
+
+  readable.on('data', (chunk) => {
+    htmlFile = chunk.toString().replace('{{header}}', header);
+    htmlFile = htmlFile.replace('{{articles}}', articles);
+    htmlFile = htmlFile.replace('{{footer}}', footer);
+  });
+
+  readable.on('end', async () => {
+    await fsProm.writeFile(pathToHtmlBundle, htmlFile, 'utf8');
+  });
+}
+
+async function createCssBundle() {
+  const allFiles = await fsProm.readdir(pathToCss);
+  const cssFiles = allFiles.filter(file => path.extname(file) === '.css');
+  const stream = fs.createWriteStream(pathToCssBundle, 'utf8');
+
+  streamMergeRecursive(cssFiles, stream);
+}
+
+function streamMergeRecursive(files = [], fileWriteStream) {
+  if (!files.length) {
+    return fileWriteStream.end();
+  }
+
+  const currentFile = path.resolve(pathToCss, files.shift());
+  const currentReadStream = fs.createReadStream(currentFile, 'utf8');
+
+  currentReadStream.pipe(fileWriteStream, { end: false });
+  currentReadStream.on('end', function () {
+    fileWriteStream.write('\n\n');
+    streamMergeRecursive(files, fileWriteStream);
+  });
+
+  currentReadStream.on('error', function (error) {
+    console.error(error);
+    fileWriteStream.close();
+  });
+}
+
+async function crateBuildFolder() {
+  const newFolderPath = path.join(__dirname, 'project-dist');
+  await fsProm.mkdir(newFolderPath, { recursive: true });
+}
+
+async function copyAssets(pathBundle, pathSource) {
+  await fsProm.mkdir(pathBundle, { recursive: true });
+  const files = await fsProm.readdir(pathSource);
+
+  files.forEach(async (file) => {
+    const baseFile = path.join(pathSource, file);
+    const newFile = path.join(pathBundle, file);
+    const stat = await fsProm.stat(baseFile);
+    if (stat.isDirectory()) {
+      copyAssets(newFile, baseFile);
+    } else {
+      await fsProm.copyFile(baseFile, newFile);
+    }
+  });
+}
+
+async function buildPage() {
+  await crateBuildFolder();
+  createHtmlBundle();
+  createCssBundle();
+  copyAssets(pathToAssetsBundle, pathToAssets);
+}
+
+buildPage();
