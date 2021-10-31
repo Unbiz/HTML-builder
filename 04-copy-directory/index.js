5c8c18e87c62a6f5c1bbd460d6b10c806b9b6fcd
Vladimir Bessonov <bessmy@gmail.com>
2021-10-31 13:02:31 +0300

    refactor: add deleting not actual files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,27 +1,28 @@
-const fs = require('fs/promises');
+const fs = require('fs');
+const fsProm = require('fs/promises');
 const path = require('path');
 
 const newFolderPath = path.join(__dirname, 'files-copy');
 const baseFolderPath = path.join(__dirname, 'files');
 
 async function copyFolder() {
-  // If we need to make an exact copy of the folder
+  try {
+    await fsProm.access(newFolderPath, fs.constants.W_OK);
+    const oldCopyFiles = await fsProm.readdir(newFolderPath);
+    oldCopyFiles.forEach(async (file) => {
+      const pathToFile = path.join(newFolderPath, file);
+      fsProm.unlink(pathToFile);
+    });
+  } catch {
+    await fsProm.mkdir(newFolderPath, { recursive: true });
+  }
 
-  // if (fs.access(newFolderPath)) {
-  //   const oldCopyFiles = await fs.readdir(newFolderPath);
-  //   oldCopyFiles.forEach(async (file) => {
-  //     const pathToFile = path.join(newFolderPath, file);
-  //     fs.unlink(pathToFile);
-  //   });
-  // }
-
-  await fs.mkdir(newFolderPath, { recursive: true });
-  const files = await fs.readdir(baseFolderPath);
+  const files = await fsProm.readdir(baseFolderPath);
 
   files.forEach(async (file) => {
     const baseFile = path.join(__dirname, 'files', file);
     const newFile = path.join(__dirname, 'files-copy', file);
-    await fs.copyFile(baseFile, newFile);
+    await fsProm.copyFile(baseFile, newFile);
   });
 }
 
